a8701720408ad09a1094ce402f61c1544193b1f4
Gerald Juwah <geraldjuwah@gmail.com>
2022-02-12 12:45:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -64,12 +64,6 @@ app.get('/api/:date',(req,res) => {
   res.status(200).send(result);
 });
 
-// your first API endpoint... 
-// app.get("/api/hello", function (req, res) {
-//   res.json({greeting: 'hello API'});
-// });
-
-
 
 // listen for requests :)
 var listener = app.listen(port, function () {
